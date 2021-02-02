e6fc54548b56f3eed278e6e1fbbf310b4061ec3e
popperro-001 <talkin.watermelion@gmail.com>
2021-02-02 22:34:34 +0900

    JM task 2.1.4

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,16 @@
 package koschei.models;
 
+
 public class Needle7 {
+
+    private Deth8 death;
+
+    public Needle7(Deth8 death) {
+        this.death = death;
+    }
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", в яйце иголка " + death.toString();
     }
 }
